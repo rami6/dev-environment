8a3f0e3d5a20e6d7024e9e808e018b8cfa8a384d
Miho M <30137645+rami6@users.noreply.github.com>
2024-06-16 23:57:02 -0700

    Update eslint.config.mjs to use prettier

--- eslint/eslint.config.mjs
+++ eslint/eslint.config.mjs
@@ -4,29 +4,43 @@ import tseslint from 'typescript-eslint'
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
 import pluginReactConfigJsxRuntime from 'eslint-plugin-react/configs/jsx-runtime.js'
 import stylisticTs from '@stylistic/eslint-plugin-ts'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
   {
     languageOptions: { globals: globals.browser },
     settings: {
-      'react': {
-        'version': 'detect'
-      }
-    }
+      react: {
+        version: 'detect',
+      },
+    },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  { files: ['**/*.jsx'], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  {
+    files: ['**/*.jsx'],
+    languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } },
+  },
   pluginReactConfig,
   pluginReactConfigJsxRuntime,
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.mjs'],
     plugins: {
-      '@stylistic/ts': stylisticTs
+      '@stylistic/ts': stylisticTs,
     },
     rules: {
-      'quotes': ['error', 'single', { 'avoidEscape': true }],
+      quotes: ['error', 'single', { avoidEscape: true }],
       '@stylistic/ts/semi': ['error', 'never'],
-    }
-  }
+      'prettier/prettier': [
+        'error',
+        {
+          semi: false,
+          singleQuote: true,
+          jsxSingleQuote: true,
+          trailingComma: 'es5',
+        },
+      ],
+    },
+  },
+  eslintPluginPrettierRecommended,
 ]
